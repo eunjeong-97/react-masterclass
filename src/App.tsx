05d3bd2b9acb6f5d8ff3a59bd1c3f8b4807e3aea
eunjeong-97 <eoss7777@naver.com>
2022-07-04 19:30:01 +0900

    3.7 TypeScript와 ReactJS조합 recap

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  // 만약 props.theme.bgCoolor라고 하면 존재하지 않는다고 타입스크립트가 알려준다
   background-color: ${(props) => props.theme.bgColor};
 `;
 const H1 = styled.h1`
@@ -18,9 +17,3 @@ function App() {
 }
 
 export default App;
-
-// @types/styled-components 패키지를 설치하면
-// DefinitelyTyped는 기본적으로 타입스크립트에게 styled-components 패키지가 어떤건지 알려주고
-// .d.ts 확장자를 가진 declaration file을 다운받아진다
-// 마찬가지로 theme color를 활용하기 위해서 declaration file을 새로 만들어서 설정하면 된다
-// styled.d.ts 파일 생성
--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,5 @@
 import { DefaultTheme } from "styled-components";
-// 여기서 만들 테마는 styled.d.ts파일에서 정의한 속성들하고 동일해야 한다
-// 만약 여기서 btnColor를 새로 지정하고 싶다면 styled.d.ts파일에서 아래와 같이 btnColor의 타입을 지정해주면 된다
-/*
-declare module 'styled-components' {
-    export interface DefaultTheme {
-        textColor: string;
-        bgColor: string;
-        btnColor: string; // 새로 기입
-    }
-}
 
-*/
 export const light: DefaultTheme = {
   bgColor: "white",
   textColor: "black",
