b3c4f619479ab72973d6c32a175a0a40598555bf
eunjeong-97 <eoss7777@naver.com>
2022-07-11 16:44:33 +0900

    5.15 immutability(2): 변경하려는 요소 앞뒤로 나열해서 새로운 원소와 연결하면서 배열생성 [...front, newItem, ...back]

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,24 +6,25 @@ function Todo({ text, category, id }: ITodo) {
   const DOING = "DOING";
   const DONE = "DONE";
   const setTodos = useSetRecoilState(todoState);
-  // todos를 콘솔로 확인해보면 객체타입의 item들이 묶인 array타입인데
-  // 여기서 todos라는 state를 업데이트 하려고 기존의 todos array를 mutate(변경)하면 안되고 새로운 state를 만들어야 한다
-  // id로 수정해야할 todo를 찾아서 todos array 안에 잇는 object의 index를 찾는다
-
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
-    // setTodos함수를 사용하면 todos의 값을 즉시 변경할 수 있거나
-    // 아니면 현재값(oldTodos)을 argument로 주는 function을 만들 수 있다
     setTodos((oldTodos) => {
-      // 변경하려는 todo의 index찾기
-      // findIndex의 안에서는 (함수형태의)조건을 만족시키는 todo의 index를 찾아준다
       const targetIndex = oldTodos.findIndex((todo) => todo.id === id);
-      // 기본적으로 새로운 todo를 만들어서 원래의 todo를 update해야한다
-      // 즉, 새로운 카테고리로 새로운 todo를 만들어야 한다
-      const oldTodo = oldTodos[targetIndex];
-      const newToto = { text, id, category: name };
+      //   const oldTodo = oldTodos[targetIndex];
+      // name as any라고 하면 타입스크립트에게 name은 확인하지마 날 믿으라 하는거다
+      // 하지만 이런식으로 any라고 하면 굳이 타입스크립트를 사용할 필요가 없기 때문에 이전에 카테고리값을 argument으로 하는것이 더 나을거다
+      const newToto = { text, id, category: name as any };
+      // 원소의 위치가 바뀌지 않기 위해서 배열의 요소를 교체한다
+      //   const foods = ["pizza", "mango", "potato", "kimbab"];
+      //   const front = foods.slice(0, 1); // mango의 앞부분 ['pizza']
+      //   const back = foods.slice(1 + 1); // mango의 뒷부분 ['potato', 'kimbab']
+      //   const finalResult = [...front, "감", ...back];
+      //   console.log(finalResult); // ['pizza', '감', 'potato', 'kimbab']
+
+      // 기존의 todos를 업데이트하는것이 아니라 새로운 todos를 반환한다
+      return [...oldTodos.slice(0, targetIndex), newToto, ...oldTodos.slice(targetIndex + 1)];
     });
   };
   return (
