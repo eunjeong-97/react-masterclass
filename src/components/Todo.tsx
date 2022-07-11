c4537ae16ee8193d609a7647439caeb449dd0fd1
eunjeong-97 <eoss7777@naver.com>
2022-07-11 16:28:29 +0900

    5.14 immutabilty (1): 새로운 카테고리의 todo생성

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,31 +6,25 @@ function Todo({ text, category, id }: ITodo) {
   const DOING = "DOING";
   const DONE = "DONE";
   const setTodos = useSetRecoilState(todoState);
-  // 만약 사용자가 Doing버튼을 클릭하면 인자를 통해서 Doing 버튼이 클릭된 것을 알고 싶다
+  // todos를 콘솔로 확인해보면 객체타입의 item들이 묶인 array타입인데
+  // 여기서 todos라는 state를 업데이트 하려고 기존의 todos array를 mutate(변경)하면 안되고 새로운 state를 만들어야 한다
+  // id로 수정해야할 todo를 찾아서 todos array 안에 잇는 object의 index를 찾는다
 
-  // 선택한 카테고리로 상태변경하기위해
-  // newCategory의 타입은 "TODO" | "DOING" | "DONE" 이라고 지정
-  //   const onClick = (newCategory: ITodo["category"]) => {
-  //     console.log(newCategory);
-  //    };
-  //   return (
-  //     <li>
-  //       <span>{text}</span>
-  //       {/* onClick함수에 카테고리 인자 전달 */}
-  //       {category !== "TODO" && <button onClick={() => onClick("TODO")}>TO DO</button>}
-  //       {category !== "DOING" && <button onClick={() => onClick("DOING")}>DOING</button>}
-  //       {category !== "DONE" && <button onClick={() => onClick("DONE")}>DONE</button>}
-  //     </li>
-  //   );
-
-  // onClick함수에 인자를 전달하지 않고 구현
-  // button에 이름을 붙여주고, 버튼에 onClick이벤트가 발생하면 이벤트를 잡아서
-  // 선택한 카테고리로 상태를 변경하기 위한 함수
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
-    console.log(name);
+    // setTodos함수를 사용하면 todos의 값을 즉시 변경할 수 있거나
+    // 아니면 현재값(oldTodos)을 argument로 주는 function을 만들 수 있다
+    setTodos((oldTodos) => {
+      // 변경하려는 todo의 index찾기
+      // findIndex의 안에서는 (함수형태의)조건을 만족시키는 todo의 index를 찾아준다
+      const targetIndex = oldTodos.findIndex((todo) => todo.id === id);
+      // 기본적으로 새로운 todo를 만들어서 원래의 todo를 update해야한다
+      // 즉, 새로운 카테고리로 새로운 todo를 만들어야 한다
+      const oldTodo = oldTodos[targetIndex];
+      const newToto = { text, id, category: name };
+    });
   };
   return (
     <li>
--- src/routes/Todos.tsx
+++ src/routes/Todos.tsx
@@ -1,29 +1,24 @@
-import {  useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 
 import { Container, Title } from "../components/Common";
 import CreateTodo from "../components/CreateTodo";
 import Todo from "../components/Todo";
 
-import {todoState } from '../atom';
+import { todoState } from "../atom";
 
 function Todos() {
-    const todos = useRecoilValue(todoState);
-
-    return (
-       <Container>
-         <Title>To Do List</Title>
-        <CreateTodo />
-        <ul>
-            {/* {todos.map(todo=><Todo key={todo.id} text={todo.text} id={todo.id} category={todo.category} />)} */}
-            {todos.map(todo=><Todo {...todo} key={todo.id} />)}
-        </ul>
-       </Container>
-    );
+  const todos = useRecoilValue(todoState);
+  return (
+    <Container>
+      <Title>To Do List</Title>
+      <CreateTodo />
+      <ul>
+        {todos.map((todo) => (
+          <Todo {...todo} key={todo.id} />
+        ))}
+      </ul>
+    </Container>
+  );
 }
 
 export default Todos;
-
-/*
-todos의 category를 변경하는 기능을 추가하는데 selector를 활용하면 된다
-selector를 이용하면 state로부터 데이터를 만들 수 잇다
- */
