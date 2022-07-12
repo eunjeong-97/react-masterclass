fd9749e51d8cf7252f563b1c0bde691343bf315a
eunjeong-97 <eoss7777@naver.com>
2022-07-12 14:37:17 +0900

    6.3 drag&drop(2): Droppable, Draggable컴포넌트에서 children function에 전달하는 provided
    - 공통적으로 제공: provided.innerRef
    - Draggable: provided.droppableProps
    - Draggable: provided.draggableProps, provided.dragHandleProps(드래그 핸들링 trigger)

--- src/routes/Trello.tsx
+++ src/routes/Trello.tsx
@@ -14,39 +14,48 @@ function Trello() {
     setHours(+event.currentTarget.value);
   };
 
-  // 유저가 드래그가 끝난 시점에서 불려지는 함수
+  //  드래그가 완료되고 어떻게 할지 지정을 하지 않았기 때문에 드래그를 해도 원상태로 돌아간다
   const onDragEnd = () => {};
   return (
     <div>
       <input type="number" placeholder="Minutes" value={minutes} onChange={onMinutesChange} />
       <input type="number" placeholder="Hours" value={hours} onChange={onHoursChange} />
-      {/* 드래그 & 드롭이 가능한 영역 설정 */}
       <DragDropContext onDragEnd={onDragEnd}>
-        {/* Drappable 안에 component를 넣으면 바로 사용할 수 있는 무언가를 얻는다
-        지금 당장은 보이지 않지만 일단 Draggable을 만든다
-        드롭할 수 잇는 영역 설정
-        */}
-        <Droppable droppableId="one">
-          {() => (
-            <ul>
-              {/* 드래그 가능한 요소 */}
-              <Draggable draggableId="first" index={0}>
-                {() => <li>Hello</li>}
-              </Draggable>
-              <Draggable draggableId="second" index={1}>
-                {() => <li>Hello</li>}
-              </Draggable>
-            </ul>
-          )}
-        </Droppable>
+        <div>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <ul ref={magic.innerRef} {...magic.droppableProps}>
+                <Draggable draggableId="first" index={0}>
+                  {/* Draggable컴포넌트가 provided props에서 제공하는
+                        draggableProps: 만약 요소가 기본적으로 드래그 되기를 원한다면 draggableProps를 사용하면 된다(요소 전체를 드래그하는것을 의미한다)
+                        dragHandleProps: 원하는곳 어디에서든지 카드를 집어서 드래그 할 수 있지만 때때로 코너에만 드래그할 수 있게 하고 싶은경우 Handle을 사용한다
+                        만약 유저를 하여금 li를 어떠한 위치에서든지 드래그해서 옮기도록 하고 싶다면 li에 draggableProps와 dragHandleProps를 넣어주면 된다
+                    */}
+                  {(magic) => (
+                    <li ref={magic.innerRef} {...magic.draggableProps}>
+                      {/* dragHandleProps를 주면 li가 핸들링하는데에 trigger가 된다는 걸 의미한다 
+                            즉 ✅ 버튼만 누를 수 있음
+                        */}
+                      <span {...magic.dragHandleProps}>✅</span>
+                      First
+                    </li>
+                  )}
+                </Draggable>
+                <Draggable draggableId="second" index={1}>
+                  {(magic) => (
+                    // Second 글씨자체를 드래그할 수 있음
+                    <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+                      Second
+                    </li>
+                  )}
+                </Draggable>
+              </ul>
+            )}
+          </Droppable>
+        </div>
       </DragDropContext>
     </div>
   );
 }
 
 export default Trello;
-
-// DragDropContext, Droppable, Draggable
-// <DragDropContext/>: 기본적으로 드래그 앤 드롭을 가능하게 하는 앱의 한 부분지정한다
-// 우리 앱의 전체에 적용되는게 아니라 특정지점에만 지정
-// <Droppable/>: 우리가 어떤걸 드롭할 수 있는 영역
