b4372aa210ff1aaed29037b46f9efd3f857453e1
eunjeong-97 <eoss7777@naver.com>
2022-07-12 14:00:00 +0900

    6.1 set selectors: selector에서 atom의 값을 변경할 수있다

--- src/atom/trello.ts
+++ src/atom/trello.ts
@@ -5,10 +5,17 @@ export const minuteState = atom({
   default: 0,
 });
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
   key: "hours",
   get: ({ get }) => {
     const minutes = get(minuteState);
     return minutes / 60;
+  },
+  // set은 수정하고 싶은 recoil atom을 set하는것을 도와준다
+  // 두번째로 들어올 값인 newValue는 atom의 변경된값을 나타낸다
+  set: ({ set }, newValue) => {
+    console.log(newValue); // 변경된 hoursSelector
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes); // minuteState값을 minutes로 변경시킨다
   },
 });
--- src/routes/Trello.tsx
+++ src/routes/Trello.tsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 
 import { minuteState, hourSelector } from "../atom/trello";
 
 function Trello() {
+  // atom에서 useRecoilState를 하게 되면
+  // 첫번째 인자는 atom의 값이고
+  // 두번째인자는 atom을 수정하는 함수
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  //   selector에서 useRecoilState를 하게 되면
+  // 첫번째인자로는 get property로부터 return한 값이고
+  // 두번째인자는 set property를 실행하는 함수이다
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // event.currentTarget.value의 타입은 string이지만 앞에 +를추가하면 number로 바꿔준다
-    // "1" → 1
     setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
   return (
     <div>
       <input type="number" placeholder="Minutes" value={minutes} onChange={onMinutesChange} />
-      <input type="number" placeholder="Hours" value={hours} />
+      <input type="number" placeholder="Hours" value={hours} onChange={onHoursChange} />
     </div>
   );
 }
 
 export default Trello;
-// minuteState를 가져와서 Hours에 보여지는 값이 바뀌도록 하고 싶은데
-// state를 가져와서 그 state를 수정하고 output을 내보낼때 selectors를 사용하면 된다
